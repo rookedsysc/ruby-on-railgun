eb5db63acad034b8b57baefe7f77b7d016290562
rookedsysc <rookedsysc36@gmail.com>
2025-01-19 15:33:30 +0900

    modify: update, destroy 라우팅 경로 수정 (posts/postId 부분 삭제)

--- simple-board/config/routes.rb
+++ simple-board/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts do
-        resources :comments
+        resources :comments, only: %i[index create]
       end
+      resources :comments, only: %i[update destroy]
     end
   end
 end
